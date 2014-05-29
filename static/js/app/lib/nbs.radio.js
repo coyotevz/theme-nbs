ee31b24a89ce4343610f9d7998e8e6e0c889cacc
Augusto Roccasalva <augusto@rocctech.com.ar>
2014-05-29 03:19:31 -0300

    Radio input working

--- static/js/app/lib/nbs.radio.js
+++ static/js/app/lib/nbs.radio.js
@@ -3,88 +3,91 @@ define([
 ], function($) {
   "use strict";
 
+  // RADIO CLASS DEFINITION
+  // ======================
+
+  var Radio = function(input, group) {
+    this.group = group;
+    this.input = $(input);
+    this.radio = this.input.parents('.radio, .radio-inline').first();
+    this.init();
+  };
+
+  Radio.prototype.init = function() {
+    var label = this.radio.is('label') ? this.radio : this.radio.find('label');
+    var mark = $('<div class="control-radio-checkmark">');
+    this.control = $('<div class="control-radio">');
+
+    this.input.toggleClass('radio-element', true);
+    this.control.append(mark);
+    this.input.after(this.control);
+    this.input.on('change', $.proxy(this, 'onChange'));
+
+    if (label) {
+      label.on('mouseenter mouseleave', $.proxy(this, 'onHover'));
+      label.on('mousedown mouseup', $.proxy(this, 'onMouseEvent'));
+    }
+
+    this.control.on('mouseenter mouseleave', $.proxy(this, 'onHover'));
+    this.control.on('mousedown mouseup', $.proxy(this, 'onMouseEvent'));
+    this.onChange(); // Check initial state
+
+    this.radio.data('nbs.radio', this); // Attach object to radio element
+  };
+
+  Radio.prototype.clear = function() {
+    this.control.removeClass('control-radio-checked');
+  };
+
+  Radio.prototype.onChange = function() {
+    if (this.input.prop('checked')) {
+      this.group.clearChecked();
+      this.control.addClass('control-radio-checked');
+      this.group.checked = this;
+    }
+  };
+
+  Radio.prototype.onHover = function(evt) {
+    this.control.toggleClass('hover', evt.type == "mouseenter");
+  };
+
+  Radio.prototype.onMouseEvent = function(evt) {
+    this.control.toggleClass('active', evt.type == "mousedown");
+  };
+
   // RADIO GROUP CLASS DEFINITION
   // ============================
 
   var RadioGroup = function(element) {
-    console.log('element', $(element));
+    var form, name, inputs, _this;
+    this.checked = null;
+
+    form = $(element).parents('form');
+    name = $(element).find('input[type=radio]').prop('name');
+    inputs = form.find('input[type=radio][name=' + name + ']');
+    _this = this;
+    inputs.each(function(idx, input) {
+      var i = new Radio(input, _this);
+    });
   };
+
+  RadioGroup.prototype.clearChecked = function() {
+    if (this.checked) this.checked.clear();
+  };
+
+  // PLUGIN DECLARATION
+  // ==================
 
   $.fn.nbsRadio = function(options) {
     return this.each(function() {
       var $this = $(this);
       var data = $this.data('nbs.radio');
 
-      if (!data) $this.data('nbs.radio', new RadioGroup(this));
+      if (!data) new RadioGroup(this);
     });
   };
 
+  // Activate plugin on default elements
   $(document).find('.radio, .radio-inline').nbsRadio();
-
-  $.fn.nbsRadio_ = function(options) {
-    return this.each(function() {
-      var $radio, $label, $input, $control, $mark;
-      var onClick, onHover, onMouseEvent, onChange, initRadioGroup;
-
-      $input = $(this).find('input[type=radio]');
-
-      if ($input.is('.radio-element') === false) {
-        initRadioGroup($input);
-      }
-
-      $radio = $(this);
-
-
-      $label = $radio.is('label') ? $radio : $radio.find('label');
-      $input = $radio.find('input[type="radio"]');
-      $control = $('<div class="control-radio">');
-      $mark = $('<div class="control-radio-checkmark">');
-
-      initRadioGroup = function($input) {
-        var $form, $group;
-        $form = $input.parents('form');
-        $group = $form.find('input[type=radio][name=' + $input.prop('name') + ']');
-        console.log('$group:', $group);
-      };
-
-      initRadioGroup($input);
-
-      onClick = function(evt) {
-        evt.preventDefault();
-        $input.trigger('click', evt);
-      };
-
-      onMouseEvent = function(evt) {
-        evt.preventDefault();
-        $control.toggleClass('active', evt.type == "mousedown");
-      };
-
-      onHover = function(evt) {
-        evt.preventDefault();
-        $control.toggleClass('hover', evt.type == "mouseenter");
-      };
-
-      onChange = function(evt) {
-        console.log('$input.checked', $input.attr('id'), $input.prop('checked'));
-        $control.toggleClass('control-radio-checked', $input.prop('checked'));
-      };
-
-      //$input.toggleClass('radio-element', true);
-      $control.append($mark);
-      $input.after($control);
-      $input.on('change', onChange);
-
-      if ($label) {
-        $label.on('mouseenter mouseleave', onHover);
-        $label.on('mousedown mouseup', onMouseEvent);
-      }
-
-      $control.on('click', onClick);
-      $control.on('mouseenter mouseleave', onHover);
-      $control.on('mousedown mouseup', onMouseEvent);
-      onChange();
-
-    });
-  };
 
 });
